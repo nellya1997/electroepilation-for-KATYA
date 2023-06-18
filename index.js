88d827f55d527e68850eea94d1cae863ccb35ba9
Nellya <syunyaeva5555@gmail.com>
2023-06-18 19:30:46 +0400

    добавлены все файлы

---
+++ index.js
@@ -0,0 +1,10 @@
+// let swiperWork = document.querySelectorAll('.swiper');
+
+// swiperWork.forEach(function (work){
+//     let swipers = new Swiper(work, {
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//         },
+//     })
+//     });
