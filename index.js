b4fa7294b1b9e6ef3c686ee1dc08a79939da4168
nikita guliaev <nikitavvv5757@gmail.com>
2023-06-19 17:55:24 +0400

    create sliders

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// let swiperWork = document.querySelectorAll('.swiper');
+let swiperWork = document.querySelectorAll('.swiper');
 
-// swiperWork.forEach(function (work){
-//     let swipers = new Swiper(work, {
-//         pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true,
-//         },
-//     })
-//     });
+swiperWork.forEach(function (work){
+    let swipers = new Swiper(work, {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    })
+    });
+
