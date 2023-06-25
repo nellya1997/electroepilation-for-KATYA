de8f1ab48e8bb4502e54c5bc6679474de62de01e
Nellya <syunyaeva5555@gmail.com>
2023-06-25 11:59:30 +0400

    добавила аккордеон

--- index.js
+++ index.js
@@ -40,3 +40,19 @@ burger.addEventListener('click', ()=> {
     burger.classList.toggle('open');
     body.classList.toggle('stop-scroll');
 })
+
+//FAQ - accorderon
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
