2ce866859f5bc2e1e0862ff8b18c989275bc280f
Nellya <syunyaeva5555@gmail.com>
2023-06-28 11:52:58 +0400

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// let swiperWork = document.querySelectorAll('.swiper');
 
-// swiperWork.forEach(function (work){
-//     let swipers = new Swiper(work, {
-//         pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true,
-//         },
-//         spaceBetween: 30,
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//     })
-//     });
 
 const swiper1 = new Swiper('.swiper', {
   slidesPerView: 1,
